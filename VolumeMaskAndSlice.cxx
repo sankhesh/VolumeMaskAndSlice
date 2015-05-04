b864d9d265de5984319df0dbbf6e8bd1d9dc89b2
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-05-04 12:26:47 -0400

    Initial pipeline with image mathematics multiply

--- VolumeMaskAndSlice.cxx
+++ VolumeMaskAndSlice.cxx
@@ -6,56 +6,63 @@
 #include <vtkColorTransferFunction.h>
 #include <vtkCylinder.h>
 #include <vtkGPUVolumeRayCastMapper.h>
+#include <vtkImageActor.h>
+#include <vtkImageCast.h>
 #include <vtkImageData.h>
+#include <vtkImageMapper3D.h>
+#include <vtkImageMapToColors.h>
+#include <vtkImageMathematics.h>
 #include <vtkImageReslice.h>
+#include <vtkImageShiftScale.h>
 #include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkMatrix4x4.h>
 #include <vtkNew.h>
 #include <vtkPiecewiseFunction.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkRTAnalyticSource.h>
 #include <vtkVolume.h>
 #include <vtkVolumeProperty.h>
-#include <vtkMatrix4x4.h>
-#include <vtkImageActor.h>
-#include <vtkImageMapper3D.h>
+#include <vtkXMLImageDataReader.h>
 
-int main(int, char**)
+int main(int argc, char* argv[])
 {
-  // Here, we use the RTAnalyticSource to create the vtkImageData
-  vtkNew<vtkRTAnalyticSource> wavelet;
-  wavelet->SetWholeExtent(-127, 128,
-                          -127, 128,
-                          -127, 128);
-  wavelet->SetCenter(0.0, 0.0, 0.0);
-  wavelet->Update();
+  if (argc < 2)
+    {
+    std::cerr << "Usage: " << argv[0] << " <input (*.vti)>" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  vtkNew<vtkXMLImageDataReader> reader;
+  reader->SetFileName(argv[1]);
+  reader->Update();
 
   double origin[3], spacing[3];
   int dims[3], extent[6];
-  wavelet->GetOutput()->GetOrigin(origin);
-  wavelet->GetOutput()->GetSpacing(spacing);
-  wavelet->GetOutput()->GetDimensions(dims);
-  wavelet->GetOutput()->GetExtent(extent);
-
-  vtkNew<vtkImageData> mask;
-  mask->SetExtent(extent);
-  mask->SetOrigin(origin);
-  mask->SetSpacing(spacing);
-  mask->SetDimensions(dims);
-  mask->AllocateScalars(VTK_UNSIGNED_CHAR, 1);
-
-  char * ptr = static_cast<char *> (mask->GetScalarPointer(0,0,0));
-
-  double radius = dims[0]/2.0 - 10;
+  reader->GetOutput()->GetOrigin(origin);
+  reader->GetOutput()->GetSpacing(spacing);
+  reader->GetOutput()->GetDimensions(dims);
+  reader->GetOutput()->GetExtent(extent);
 
   double center[3];
   for (int i = 0; i < 3; ++i)
     {
     center[i] = origin[i] + spacing[i] * 0.5 * (extent[2*i] + extent[2*i+1]);
     }
+
+  vtkNew<vtkImageData> mask;
+  mask->SetDimensions(dims);
+  mask->SetOrigin(extent[0], extent[2], extent[4]);
+  mask->SetSpacing(spacing);
+  mask->SetExtent(extent);
+  mask->AllocateScalars(VTK_UNSIGNED_CHAR, 1);
+
+  char * ptr = static_cast<char *> (mask->GetScalarPointer(0,0,0));
+
+  double radius = dims[0]/2.0 - 40;
+
   vtkNew<vtkCylinder> cylinder;
-  cylinder->SetCenter(mask->GetCenter());
+  cylinder->SetCenter(center);
   cylinder->SetRadius(radius);
 
   for ( int z = 0; z < dims[2]; ++z)
@@ -64,7 +71,7 @@ int main(int, char**)
       {
       for (int x = 0; x < dims[0]; ++x)
         {
-        if (cylinder->vtkImplicitFunction::EvaluateFunction(x,y,z) > 0)
+        if (cylinder->vtkImplicitFunction::EvaluateFunction(x,z,y) > 0)
           {
           // point is outide cylinder
           *ptr++ = 0;
@@ -87,40 +94,66 @@ int main(int, char**)
   resliceAxes->DeepCopy(elements);
 
   vtkNew<vtkImageReslice> reslice;
-  reslice->SetInputConnection(wavelet->GetOutputPort());
+  reslice->SetInputConnection(reader->GetOutputPort());
   reslice->SetOutputDimensionality(2);
-  reslice->SetResliceAxesDirectionCosines( 1, 0, 0,
-                                           0,-1, 0,
-                                           0, 0,-1);
-  reslice->SetResliceAxesOrigin(mask->GetCenter());
+  reslice->SetResliceAxesDirectionCosines( 0,-1, 0,
+                                           0, 0,-1,
+                                          -1, 0,0);
+  reslice->SetResliceAxesOrigin(center);
   reslice->SetInterpolationModeToLinear();
   reslice->Update();
 
-  vtkNew<vtkImageData> reslicedWavelet;
-  reslicedWavelet->DeepCopy(reslice->GetOutput());
+  vtkNew<vtkImageData> reslicedVolume;
+  reslicedVolume->DeepCopy(reslice->GetOutput());
 
   reslice->SetInputData(mask.GetPointer());
+  reslice->SetResliceAxesOrigin(center);
   reslice->Update();
   vtkNew<vtkImageData> reslicedMask;
   reslicedMask->DeepCopy(reslice->GetOutput());
 
-  vtkNew<vtkImageActor> slice;
-  slice->GetMapper()->SetInputData(reslicedWavelet.GetPointer());
+  vtkNew<vtkImageCast> cast;
+  cast->SetInputData(reslicedMask.GetPointer());
+  cast->SetOutputScalarType(reslicedVolume->GetScalarType());
+  cast->Update();
+
+  vtkNew<vtkImageMathematics> imMath;
+  imMath->SetInput1Data(reslicedVolume.GetPointer());
+  imMath->SetInput2Data(cast->GetOutput());
+  imMath->SetOperationToMultiply();
+  imMath->Update();
+  vtkImageData* mathOutput = imMath->GetOutput();
+
+  double originalRange[2], newRange[2];
+  reslicedVolume->GetScalarRange(originalRange);
+  mathOutput->GetScalarRange(newRange);
+  double scale = originalRange[1] / newRange[1];
+
+  vtkNew<vtkImageShiftScale> shiftScale;
+  shiftScale->SetInputConnection(imMath->GetOutputPort());
+  shiftScale->SetShift(0.0);
+  shiftScale->SetScale(scale);
+  shiftScale->SetOutputScalarType(reslicedVolume->GetScalarType());
 
   vtkNew<vtkGPUVolumeRayCastMapper> volumeMapper;
-  volumeMapper->SetInputConnection(wavelet->GetOutputPort());
+  volumeMapper->SetInputConnection(reader->GetOutputPort());
   volumeMapper->SetMaskInput(mask.GetPointer());
   volumeMapper->SetMaskTypeToBinary();
 
   vtkNew<vtkVolumeProperty> volumeProperty;
   vtkNew<vtkColorTransferFunction> ctf;
-  ctf->AddRGBPoint(37.3531, 0.2, 0.29, 1);
-  ctf->AddRGBPoint(157.091, 0.87, 0.87, 0.87);
-  ctf->AddRGBPoint(276.829, 0.7, 0.015, 0.15);
+  ctf->AddRGBPoint(0.0, 0.31, 0.34, 0.43);
+  ctf->AddRGBPoint(556.24, 0, 0.0, 1);
+  ctf->AddRGBPoint(1112.48, 0, 1, 1);
+  ctf->AddRGBPoint(1636, 0, 1, 0);
+  ctf->AddRGBPoint(2192.24, 1, 1, 0);
+  ctf->AddRGBPoint(2748.48, 1, 0, 0);
+  ctf->AddRGBPoint(3272, 0.88, 0, 1);
 
   vtkNew<vtkPiecewiseFunction> pwf;
-  pwf->AddPoint(37.3531, 0.0);
-  pwf->AddPoint(276.829, 0.02);
+  pwf->AddPoint(0.0, 0.0);
+  //pwf->AddPoint(1479.69, 0.139);
+  pwf->AddPoint(3272, 1);
 
   volumeProperty->SetColor(ctf.GetPointer());
   volumeProperty->SetScalarOpacity(pwf.GetPointer());
@@ -129,18 +162,32 @@ int main(int, char**)
   volume->SetMapper(volumeMapper.GetPointer());
   volume->SetProperty(volumeProperty.GetPointer());
 
+  vtkNew<vtkImageMapToColors> lut;
+  //lut->SetInputData(reslicedVolume.GetPointer());
+  lut->SetInputConnection(imMath->GetOutputPort());
+  lut->SetLookupTable(ctf.GetPointer());
+
+  vtkNew<vtkImageActor> slice;
+  slice->GetMapper()->SetInputConnection(lut->GetOutputPort());
+
   vtkNew<vtkRenderWindow> renWin;
-  renWin->SetSize(400,400);
+  renWin->SetSize(600,600);
   vtkNew<vtkRenderWindowInteractor> iren;
   iren->SetRenderWindow(renWin.GetPointer());
   vtkNew<vtkInteractorStyleTrackballCamera> style;
   iren->SetInteractorStyle(style.GetPointer());
-  vtkNew<vtkRenderer> ren;
-  renWin->AddRenderer(ren.GetPointer());
 
-  ren->AddVolume(volume.GetPointer());
-  ren->AddActor(slice.GetPointer());
-  ren->ResetCamera();
+  vtkNew<vtkRenderer> ren1;
+  ren1->SetViewport(0,0,0.5,1);
+  renWin->AddRenderer(ren1.GetPointer());
+  vtkNew<vtkRenderer> ren2;
+  ren2->SetViewport(0.5,0,1,1);
+  renWin->AddRenderer(ren2.GetPointer());
+
+  ren1->AddVolume(volume.GetPointer());
+  ren1->ResetCamera();
+  ren2->AddActor(slice.GetPointer());
+  ren2->ResetCamera();
 
   renWin->Render();
   iren->Initialize();
